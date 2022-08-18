e718bb596fd9f2ac05e190ba562f5575839fcda0
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 12:17:42 +0000

    Create sx_list.dart

---
+++ lib/src/types/sx_list.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:convert';
+import 'package:state_variable/src/enums/enums.dart';
+import 'package:state_variable/src/state_variable.dart';
+
+class SxList<T> extends StateVariable<List<T>> {
+  ///
+  const SxList({required super.value, super.status, super.error});
+
+  @override
+  StateVariable<List<T>> fromJson(Map<String, dynamic> json) {
+    return SxList<T>(
+      value: json['value'] as List<T>,
+      status: Status.values[json['status'] as int],
+      error: json['error'] as String,
+    );
+  }
+
+  @override
+  StateVariable<List<T>> fromMap(Map<String, dynamic> map) {
+    return fromJson(map);
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'value': value,
+      'error': error ?? '',
+      'status': status.index,
+    };
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return toJson();
+  }
+
+  @override
+  String toStringJson() {
+    return json.encode(toJson());
+  }
+
+  ///
+  @override
+  SxList<T> toInitial([List<T>? value]) => SxList<T>(
+        value: value ?? this.value,
+      );
+
+  ///
+  @override
+  SxList<T> toLoading([List<T>? value]) => SxList<T>(
+        value: value ?? this.value,
+        status: Status.loading,
+      );
+
+  ///
+  @override
+  SxList<T> toRefreshing([List<T>? value]) => SxList<T>(
+        value: value ?? this.value,
+        status: Status.refresh,
+      );
+
+  ///
+  @override
+  SxList<T> toSuccess([List<T>? value]) => SxList<T>(
+        value: value ?? this.value,
+        status: Status.success,
+      );
+
+  ///
+  @override
+  SxList<T> toFailed([List<T>? value, String? errorMessage]) => SxList<T>(
+        value: value ?? this.value,
+        status: Status.failed,
+        error: errorMessage,
+      );
+
+  @override
+  String toString() {
+    return 'StateVariable('
+        'status: $this.status, '
+        'value: $value, '
+        'hasError:'
+        '${(error != null && (error?.isNotEmpty)!) || isFailed})';
+  }
+
+  @override
+  List<Object?> get props => super.props;
+}
