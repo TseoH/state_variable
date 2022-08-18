919517cb7d7457fc6cbe1732b562bca3635930b0
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 08:33:29 +0000

    Update status.dart

--- lib/src/enums/status.dart
+++ lib/src/enums/status.dart
@@ -1,16 +1,52 @@
+///
 extension StatusExtension on Status {
-  //
+  ///
   Status toggleToInitial() => Status.initial;
+
+  ///
   Status toggleToLoading() => Status.loading;
+
+  ///
   Status toggleToRefreshing() => Status.refresh;
+
+  ///
   Status toggleToFailed() => Status.failed;
+
+  ///
   Status toggleToSuccess() => Status.success;
-  //
+
+  ///
   bool get isInitial => this == Status.initial;
+
+  ///
   bool get isLoading => this == Status.loading;
+
+  ///
   bool get isRefreshing => this == Status.refresh;
+
+  ///
   bool get isSucceeded => this == Status.success;
+
+  ///
   bool get isFailed => this == Status.failed;
 }
 
-enum Status { initial, loading, success, failed, refresh }
+///
+enum Status {
+  ///
+  initial,
+
+  ///
+  loading,
+
+  ///
+  success,
+
+  ///
+  failed,
+
+  ///
+  refresh;
+
+  ///
+}
