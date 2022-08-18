d177523e32894db828e66dbd5b854be23001343c
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 13:36:23 +0000

    Create get_t_from_json.dart

---
+++ lib/src/helpers/get_t_from_json.dart
@@ -0,0 +1,15 @@
+///
+T getTFromJson<T>(Map<String, dynamic> json) {
+  final onThrow = UnimplementedError('$T need to implement '
+      'a factory or method with exact name like fromJson or fromMap');
+  final data = json['value'] as Map<String, dynamic>;
+  try {
+    return (T as dynamic).fromJson(data) as T;
+  } catch (_) {
+    try {
+      return (T as dynamic).fromMap(data) as T;
+    } catch (_) {
+      throw onThrow;
+    }
+  }
+}
