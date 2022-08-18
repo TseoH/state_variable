cf4c854d75fb6d6b201fc9e2bd0208174f73ea15
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 12:50:53 +0000

    Update types.dart

--- lib/src/types/types.dart
+++ lib/src/types/types.dart
@@ -1,7 +1,8 @@
 //GENERATED BARREL FILE
+export 'sx.dart';
+export 'sx_double.dart';
 export 'sx_int.dart';
+export 'sx_list.dart';
 export 'sx_map.dart';
 export 'sx_num.dart';
-export 'sx_list.dart';
-export 'sx_double.dart';
 export 'sx_string.dart';
