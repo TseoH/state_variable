00ae26e4fc3fa27f18a8226995defc754f1c6cbe
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 12:17:45 +0000

    Create sx_map.dart

---
+++ lib/src/types/sx_map.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:convert';
+import 'package:state_variable/src/enums/enums.dart';
+import 'package:state_variable/src/state_variable.dart';
+
+class SxMap<T, P> extends StateVariable<Map<T, P>> {
+  ///
+  const SxMap({required super.value, super.status, super.error});
+
+  @override
+  StateVariable<Map<T, P>> fromJson(Map<String, dynamic> json) {
+    return SxMap<T, P>(
+      value: json['value'] as Map<T, P>,
+      status: Status.values[json['status'] as int],
+      error: json['error'] as String,
+    );
+  }
+
+  @override
+  StateVariable<Map<T, P>> fromMap(Map<String, dynamic> map) {
+    return fromJson(map);
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'value': value,
+      'error': error ?? '',
+      'status': status.index,
+    };
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return toJson();
+  }
+
+  @override
+  String toStringJson() {
+    return json.encode(toJson());
+  }
+
+  ///
+  @override
+  SxMap<T, P> toInitial([Map<T, P>? value]) => SxMap<T, P>(
+        value: value ?? this.value,
+      );
+
+  ///
+  @override
+  SxMap<T, P> toLoading([Map<T, P>? value]) => SxMap<T, P>(
+        value: value ?? this.value,
+        status: Status.loading,
+      );
+
+  ///
+  @override
+  SxMap<T, P> toRefreshing([Map<T, P>? value]) => SxMap<T, P>(
+        value: value ?? this.value,
+        status: Status.refresh,
+      );
+
+  ///
+  @override
+  SxMap<T, P> toSuccess([Map<T, P>? value]) => SxMap<T, P>(
+        value: value ?? this.value,
+        status: Status.success,
+      );
+
+  ///
+  @override
+  SxMap<T, P> toFailed([Map<T, P>? value, String? errorMessage]) => SxMap<T, P>(
+        value: value ?? this.value,
+        status: Status.failed,
+        error: errorMessage,
+      );
+
+  @override
+  String toString() {
+    return 'StateVariable('
+        'status: $this.status, '
+        'value: $value, '
+        'hasError:'
+        '${(error != null && (error?.isNotEmpty)!) || isFailed})';
+  }
+
+  @override
+  List<Object?> get props => super.props;
+}
