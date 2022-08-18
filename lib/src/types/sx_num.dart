e6b84fd84adc9043e235c85d7a4714e2cd6e76b5
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-18 12:17:24 +0000

    Create sx_num.dart

---
+++ lib/src/types/sx_num.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:convert';
+import 'package:state_variable/src/enums/enums.dart';
+import 'package:state_variable/src/state_variable.dart';
+
+class SxNum extends StateVariable<num> {
+  ///
+  const SxNum({required super.value, super.status, super.error});
+
+  @override
+  StateVariable<num> fromJson(Map<String, dynamic> json) {
+    return SxNum(
+      value: json['value'] as num,
+      status: Status.values[json['status'] as int],
+      error: json['error'] as String,
+    );
+  }
+
+  @override
+  StateVariable<num> fromMap(Map<String, dynamic> map) {
+    return fromJson(map);
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'value': value,
+      'error': error ?? '',
+      'status': status.index,
+    };
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return toJson();
+  }
+
+  @override
+  String toStringJson() {
+    return json.encode(toJson());
+  }
+
+  ///
+  @override
+  SxNum toInitial([num? value]) => SxNum(
+        value: value ?? this.value,
+      );
+
+  ///
+  @override
+  SxNum toLoading([num? value]) => SxNum(
+        value: value ?? this.value,
+        status: Status.loading,
+      );
+
+  ///
+  @override
+  SxNum toRefreshing([num? value]) => SxNum(
+        value: value ?? this.value,
+        status: Status.refresh,
+      );
+
+  ///
+  @override
+  SxNum toSuccess([num? value]) => SxNum(
+        value: value ?? this.value,
+        status: Status.success,
+      );
+
+  ///
+  @override
+  SxNum toFailed([num? value, String? errorMessage]) => SxNum(
+        value: value ?? this.value,
+        status: Status.failed,
+        error: errorMessage,
+      );
+
+  @override
+  String toString() {
+    return 'StateVariable('
+        'status: $this.status, '
+        'value: $value, '
+        'hasError:'
+        '${(error != null && (error?.isNotEmpty)!) || isFailed})';
+  }
+
+  @override
+  List<Object?> get props => super.props;
+}
