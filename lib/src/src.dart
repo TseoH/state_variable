ac83a2b8c582c30568090b11dce8919ae465e6dc
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-08-26 12:20:58 +0000

    Update barrels

--- lib/src/src.dart
+++ lib/src/src.dart
@@ -1,4 +1,4 @@
 //GENERATED BARREL FILE
-export 'state_variable.dart';
 export './enums/enums.dart';
 export './types/types.dart';
+export 'state_variable.dart';
--- lib/src/types/types.dart
+++ lib/src/types/types.dart
@@ -1,9 +1,9 @@
 //GENERATED BARREL FILE
 export 'sx.dart';
+export 'sx_bool.dart';
 export 'sx_double.dart';
 export 'sx_int.dart';
 export 'sx_list.dart';
 export 'sx_map.dart';
 export 'sx_num.dart';
 export 'sx_string.dart';
-export 'sx_bool.dart';
